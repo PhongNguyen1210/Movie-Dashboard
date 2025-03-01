99e940b5dbde8ad5edda23b7e63a272593b29408
PhongNguyen1210 <nguyendangthanhphong.lop8a7.23@gmail.com>
2025-03-01 15:30:03 +0700

    upload source code

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav'),
+      headerMenu = document.getElementById('header-menu'),
+      navClose = document.getElementById('nav-close')
+
+/*===== Menu show =====*/
+if(headerMenu){
+    headerMenu.addEventListener('click', () =>{
+        nav.classList.add('show-menu')
+    })
+}
+
+/*===== Menu hidden =====*/
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        nav.classList.remove('show-menu')
+    })
+}
+
+
+/*=============== SWIPER MOVIE ===============*/
+
+
+/*=============== SWIPER NEW ===============*/
+
+
+/*=============== ADD BLUR HEADER ===============*/
+
