dd871066db76f0b4e9aaf8ffe69fa413e34d01a3
PhongNguyen1210 <nguyendangthanhphong.lop8a7.23@gmail.com>
2025-03-02 20:03:43 +0700

    continues

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,24 @@ if(navClose){
 
 
 /*=============== SWIPER MOVIE ===============*/
+let swiperMovie = new Swiper('.movie__swiper', {
+    loop: true,
+    grabCursor: true,
+    sliderPerView: 2,
+    spaceBetween: 24,
 
+    breakpoints:{
+        440:{
+            slidesPerView: 'auto',
+        },
+        768:{
+            slidesPerView: 4,
+        },
+        1200:{
+            slidesPerView: 5,
+        }
+    }
+})
 
 /*=============== SWIPER NEW ===============*/
 
